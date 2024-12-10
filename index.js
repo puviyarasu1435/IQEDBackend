fe64495dc6658a54d76e741eb840706eb7347cd2
gowthamrajvp <gowthamraj9344@gmail.com>
2024-12-10 18:52:56 +0530

    d

--- index.js
+++ index.js
@@ -32,11 +32,13 @@ const { main } = require("./app/Stream/User.Stream");
 
 // Middleware
 app.use(
-  cors({
-    // origin: "http://localhost:5173", 
-    // methods: ["GET", "POST","PUT"],
-    credentials: true, 
-  })
+  cors(
+  //   {
+  //   // origin: "http://localhost:5173", 
+  //   // methods: ["GET", "POST","PUT"],
+  //   // credentials: true, 
+  // }
+)
 );
 app.use(express.json({limit: '50mb'}));
 app.use(sessionMiddleware);
