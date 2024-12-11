e22411e912c88f83f6abb35e16a06540647ab8a7
Puviyarasu <puviyarasu5341@gmail.com>
2024-12-11 06:21:46 +0530

    cors

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const { main } = require("./app/Stream/User.Stream");
 
 // app.use(cors(corsOptions));
 app.use(cors({
-  origin: 'https://iqed-iq.vercel.app/', 
+  origin: '*', 
   methods: 'GET,POST,OPTIONS',
   credentials: true,
 }));
