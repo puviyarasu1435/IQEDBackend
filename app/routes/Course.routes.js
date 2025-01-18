a3b26836429b1c4f89d7ade1f49eafe0c21a77f6
Puviyarasu <puviyarasu5341@gmail.com>
2025-01-18 22:39:15 +0530

    vvv

--- app/routes/Course.routes.js
+++ app/routes/Course.routes.js
@@ -1,14 +1,27 @@
-const { newCourse,GetCourse,GetCourseById } = require("../controllers/Course.controller");
 const {
-    getAllTopics,
-    getTopicById,
-    updateTopic,
-    deleteTopic,
-    postTopicsInBulk,
-  } = require("../controllers/Topic.controller");
+  newCourse,
+  GetCourse,
+  GetCourseById,
+} = require("../controllers/Course.controller");
+const {   postQuestions,
+  putQuestions,
+  deleteQuestions,
+  getAllQuestions,
+  getQuestions, } = require("../controllers/Question.controller");
+const {
+  getAllTopics,
+  getTopicById,
+  updateTopic,
+  deleteTopic,
+  postTopicsInBulk,
+} = require("../controllers/Topic.controller");
 
-  const router = require("express").Router();
-
+const router = require("express").Router();
+router.get("/question:id", getQuestions);
+router.get("/question", deleteQuestions);
+router.post("/question", postQuestions);
+router.put("/question:id", putQuestions);
+router.delete("/question:id", deleteQuestions);
 router.post("/topics", postTopicsInBulk);
 router.get("/topics", getAllTopics);
 router.get("/topics/:id", getTopicById);
@@ -19,6 +32,5 @@ router.get("/courses", GetCourse);
 router.get("/courses/:id", GetCourseById);
 
 // router.post("/quest", createQuest);
-
 
 module.exports = router;
