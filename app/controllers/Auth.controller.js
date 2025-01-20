b57e487a8f3a1a5e835adef7d7caafcb0fd3fdc8
gowthamrajvp <gowthamraj9344@gmail.com>
2025-01-20 08:24:05 +0530

    s

--- app/controllers/Auth.controller.js
+++ app/controllers/Auth.controller.js
@@ -109,7 +109,7 @@ async function UserSignIn(req, res) {
 
     req.session.Token = token;
 
-    return res.status(200).json({ message: "Authentication successful" });
+    return res.status(200).json({ message: "Authentication successful",token });
   } catch (error) {
     console.error("Error during authentication:", error);
     return res.status(500).send("An error occurred. Please try again.");
