8a2e18580cbbaf912de5a2ac93f575d6c5016c4a
Puviyarasu <puviyarasu5341@gmail.com>
2025-01-20 08:21:38 +0530

    token chnage

--- app/config/jwt.config.js
+++ app/config/jwt.config.js
@@ -1,7 +1,14 @@
 const JWT = require("jsonwebtoken");
 
 function jwt_isVerify(req, res, next) {
-  const UserToken = req.session.Token;
+  let UserToken = req.session.Token;
+  const authHeader = req.headers['authorization'];
+  if (!authHeader) {
+    return res.status(401).json({ message: 'Authorization header missing' });
+  }
+
+  // Extract the token (Assumes 'Bearer <token>' format)
+  UserToken = authHeader.split(' ')[1];
   if(UserToken!=undefined){
     JWT.verify(UserToken, process.env.JWT_Token, (err, Data) => {
         if (err) return res.status(403).json({ message: "UnAuthorized" });
@@ -11,7 +18,6 @@ function jwt_isVerify(req, res, next) {
   }else{
     res.status(403).json({ message: "UnAuthorized" });
   }
-
 }
 
 function jwt_GetToken(Data, ExpireTime = "1h") {
