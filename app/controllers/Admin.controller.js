c27a5e3b2065055e1e428eecc9083dce627ce9d6
Puviyarasu <puviyarasu5341@gmail.com>
2025-02-12 22:20:21 +0530

    feed

--- app/controllers/Admin.controller.js
+++ app/controllers/Admin.controller.js
@@ -1,9 +1,29 @@
 const express = require("express");
 
 const UserModel = require("../models/User/User.model"); // Assuming User model is in models/User.js
+const { PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
+const S3 = require("../config/aws.config");
 
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const FeedbackModel = require("../models/User/Feedback.model");
+const BUCKET_NAME = process.env.S3_BUCKET_NAME;
 
+async function uploadImagesToS3(file) {
+  if (!file || !file.buffer || !file.originalname || !file.mimetype) {
+    throw new Error("Invalid file object. Ensure file is correctly uploaded.");
+  }
+  const fileKey = `${Date.now()}-${file.originalname}`;
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: fileKey,
+    Body: file.buffer,
+    ContentType: file.mimetype,
+  };
 
+  await S3.send(new PutObjectCommand(params));
+
+  return fileKey;
+}
 // Get last 30 days active user count
 async function getActiveUsersLast30Days(req, res) {
   try {
@@ -72,21 +92,46 @@ async function getUsersCreatedLastWeek(req, res) {
   }
 }
 
+async function generateSignedUrl(imageKey) {
+  return await getSignedUrl(
+    S3,
+    new GetObjectCommand({
+      Bucket: BUCKET_NAME,
+      Key: imageKey,
+    }),
+    { expiresIn: 60 * 60 * 24 } // URL expires in 1 hour
+  );
+}
 // Get all users without auth.password
 async function getAllUsers(req, res) {
   try {
     const users = await UserModel.find({}, { "auth.password": 0 });
-    res.json({ users });
+
+    const usersWithProfileUrl = await Promise.all(
+      users.map(async (user) => {
+        const profileUrl = user.profileImage
+          ? await generateSignedUrl(user.profileImage)
+          : null;
+        
+        return {
+          ...user.toObject(),
+          profileImage: profileUrl,
+        };
+      })
+    );
+
+    res.json({ users: usersWithProfileUrl });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 }
 
+
 // Delete multiple users by _id
 async function deleteUsers(req, res) {
   try {
     const { userIds } = req.body; // Expecting an array of user _id's
-    console.log("Id",userIds)
+    console.log("Id", userIds);
     if (!Array.isArray(userIds) || userIds.length === 0) {
       return res.status(400).json({ error: "Invalid user IDs" });
     }
@@ -97,11 +142,137 @@ async function deleteUsers(req, res) {
       return res.status(404).json({ message: "No users found to delete" });
     }
 
-    res.json({ message: "Users deleted successfully", deletedCount: result.deletedCount });
+    res.json({
+      message: "Users deleted successfully",
+      deletedCount: result.deletedCount,
+    });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 }
 
 
-module.exports = {getActiveUsersLast30Days,getTotalUsers,getUsersCreatedThisWeek,getUsersCreatedLastWeek,getAllUsers,deleteUsers};
+async function UpdateUser(req, res) {
+  try {
+    const {
+      userId,
+      name,
+      userName,
+      email,
+      age,
+      mobileNumber,
+      parentsName,
+      schoolName,
+      grade,
+    } = req.body;
+    console.log("Received Files:", req.file);
+    console.log("Received Body:", req.body);
+    if (!userId) {
+      return res.status(400).json({ message: "User ID is required" });
+    }
+    const user = await UserModel.findById(userId);
+    if (!userId) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (name) user.name = name;
+    if (userName) user.schoolName = userName;
+    if (email) user.auth.email = email;
+    if (age) user.age = age;
+    if (mobileNumber) user.mobileNumber = mobileNumber;
+    if (parentsName) user.parentsName = parentsName;
+    if (schoolName) user.schoolName = schoolName;
+    if (grade) user.grade = grade;
+
+    if (req.file) {
+      let profileImageFile = Array.isArray(req.file) ? req.file[0] : req.file;
+      let profileImage = await uploadImagesToS3(profileImageFile);
+      if (profileImage) {
+        user.profileImage = profileImage;
+        console.log("Uploaded Profile Image:", profileImage);
+      }
+    }
+
+    await user.save();
+
+    const profileUrl = user.profileImage
+      ? await generateSignedUrl(user.profileImage)
+      : null;
+
+    res.status(200).json({
+      message: "User profile updated successfully",
+      data: {
+        name: user.name,
+        email: user.auth.email,
+        profile: profileUrl,
+        username: user.userName,
+        age: user.age,
+        schoolname: user.schoolName,
+        grade: user.grade,
+        mobilenumber: user.mobileNumber,
+        earnings: user.earnings,
+      },
+    });
+  } catch (error) {
+    console.error("Error updating user profile:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+}
+
+
+
+async function deleteFeedback(req, res) {
+  try {
+    const { feedIds } = req.body; // Expecting an array of user _id's
+    console.log("Id", feedIds);
+    if (!Array.isArray(feedIds) || feedIds.length === 0) {
+      return res.status(400).json({ error: "Invalid user IDs" });
+    }
+
+    const result = await FeedbackModel.deleteMany({ _id: { $in: feedIds } });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ message: "No users found to delete" });
+    }
+
+    res.json({
+      message: "Users deleted successfully",
+      deletedCount: result.deletedCount,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+  getActiveUsersLast30Days,
+  getTotalUsers,
+  getUsersCreatedThisWeek,
+  getUsersCreatedLastWeek,
+  getAllUsers,
+  deleteUsers,
+  UpdateUser,
+  deleteFeedback
+};
